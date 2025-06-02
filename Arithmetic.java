08085ae022e77c9930d8019a2a0eb188b58b731c
Anujin <aneku3185gmail.com>
2025-06-02 13:46:43 +0800

    Initial commit

---
+++ Arithmetic.java
@@ -0,0 +1,6 @@
+public class Arithmetic {
+    int add(int a, int b) {
+     int c= a+b;
+     return c;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    Person person1= new Person();
+    person1.setName("Anujin");
+    person1.setAge(18);
+    System.out.println(person1.getName()+ " " +person1.getAge()+ "tai");
+
+   person1.greet();
+  }
+  
+}
